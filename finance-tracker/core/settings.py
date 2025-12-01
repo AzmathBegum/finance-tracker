ce0aa406eda66c24a2a6f290b5c1049d2e9ebfb5
Azmath Begum <23eg106d02@anurag.edu.in>
2025-12-01 16:37:05 +0530

    Add Render domain to ALLOWED_HOSTS

--- finance-tracker/core/settings.py
+++ finance-tracker/core/settings.py
@@ -30,7 +30,12 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "finance-tracker-1-5akg.onrender.com",
+]
+
 
 
 # Application definition
